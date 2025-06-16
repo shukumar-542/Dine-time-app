935b06d253a95db437cbb0e6d5f07c539e9ac7b7
shukumar-542 <shukumar542@gmail.com>
2025-06-16 09:22:58 +0600

    Implement flash screen

---
+++ app/(auth)/signup.jsx
@@ -0,0 +1,50 @@
+import {
+    Image,
+    Pressable,
+    ScrollView,
+    StatusBar,
+    Text,
+    View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import logo from "../../assets/images/dinetimelogo.png";
+import frame from "../../assets/images/Frame.png";
+const Signup = () => {
+  return (
+    <SafeAreaView className={`bg-[#242424]`}>
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="m-2 flex justify-center items-center">
+          <Image source={logo} style={{ width: 300, height: 300 }} />
+          <Text className="text-lg text-center text-white mb-10 font-bold">
+            Let's get you started
+          </Text>
+          
+        </View>
+        <View className="flex-1">
+          <Image
+            source={frame}
+            className="w-full h-full"
+            resizeMode="contain"
+          />
+        </View>
+        <Pressable
+          onPress={() => router.push("/home")}
+          className="bg-[#f49b33] text-black p-2 rounded-md "
+        >
+          <Text className="text-center  text-xl">Sign Up</Text>
+        </Pressable>
+
+        <Pressable
+          className="flex flex-row items-center justify-center gap-2  "
+          onPress={() => router.push("/signin")}
+        >
+          <Text className="text-white ">Already have account ?</Text>
+          <Text className="text-[#f49b33] text-xl">Signin</Text>
+        </Pressable>
+        <StatusBar barStyle={"light-content"} />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Signup;
--- app/index.jsx
+++ app/index.jsx
@@ -1,28 +1,57 @@
 import { router } from "expo-router";
-import { Image, Pressable, ScrollView, StatusBar, Text, View } from "react-native";
+import {
+  Image,
+  Pressable,
+  ScrollView,
+  StatusBar,
+  Text,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import logo from "../assets/images/dinetimelogo.png";
+import frame from "../assets/images/Frame.png";
 import "../global.css";
 export default function Index() {
   return (
     <SafeAreaView className={`bg-[#242424]`}>
       <StatusBar barStyle={"light-content"} />
-      <ScrollView contentContainerStyle={{height : "100%"}}>
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
         <View className="m-2 flex justify-center items-center">
-          <Image source={logo} style={{width : 300 , height : 300}} />
+          <Image source={logo} style={{ width: 300, height: 300 }} />
           <View className="w-3/4">
-            <Pressable onPress={()=> router.push('/home')} className="bg-[#f49b33] text-black p-2 rounded-md ">
+            <Pressable
+              onPress={() => router.push("/signup")}
+              className="bg-[#f49b33] text-black p-2 rounded-md "
+            >
               <Text className="text-center  text-xl">Sign Up</Text>
             </Pressable>
-          
-            <Pressable onPress={()=> router.push('/home')} className="border my-2  border-[#f49b33] p-2 rounded-md ">
-              <Text className="text-center text-[#f49b33]  text-xl">Guest User</Text>
+
+            <Pressable
+              onPress={() => router.push("/home")}
+              className="border my-2  border-[#f49b33] p-2 rounded-md "
+            >
+              <Text className="text-center text-[#f49b33]  text-xl">
+                Guest User
+              </Text>
             </Pressable>
           </View>
+          <Text className="text-white flex flex-row items-center justify-center my-5">
+            <View className="border-b-2 border-[#f49b33] w-24" />
+           {" "} Or{" "}
+            <View className="border-b-2 border-[#f49b33] w-24" />
+          </Text>
+          <Pressable
+            className="flex flex-row items-center gap-2  "
+            onPress={() => router.push("/signin")}
+          >
+            <Text className="text-white ">Already have account ?</Text>
+            <Text className="text-[#f49b33] text-xl">Signin</Text>
+          </Pressable>
+        </View>
+        <View className="flex-1">
+          <Image source={frame} className="w-full h-full"  resizeMode="contain" />
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 }
-
-

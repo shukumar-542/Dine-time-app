a3554b007363d9ab366fbe863c565d71a546aa32
shukumar-542 <shukumar542@gmail.com>
2025-06-16 07:35:57 +0600

    Implement signup button

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,5 @@
-import { Image, ScrollView, StatusBar, View } from "react-native";
+import { router } from "expo-router";
+import { Image, Pressable, ScrollView, StatusBar, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import logo from "../assets/images/dinetimelogo.png";
 import "../global.css";
@@ -9,7 +10,15 @@ export default function Index() {
       <ScrollView contentContainerStyle={{height : "100%"}}>
         <View className="m-2 flex justify-center items-center">
           <Image source={logo} style={{width : 300 , height : 300}} />
-
+          <View className="w-3/4">
+            <Pressable onPress={()=> router.push('/home')} className="bg-[#f49b33] text-black p-2 rounded-md ">
+              <Text className="text-center  text-xl">Sign Up</Text>
+            </Pressable>
+          
+            <Pressable onPress={()=> router.push('/home')} className="border my-2  border-[#f49b33] p-2 rounded-md ">
+              <Text className="text-center text-[#f49b33]  text-xl">Guest User</Text>
+            </Pressable>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
